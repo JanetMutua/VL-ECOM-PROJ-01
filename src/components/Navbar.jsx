23f0d94e901de829c3189ccbb0bbeb68b8b523c0
janet <janetmutua04@gmail.com>
2022-06-02 20:16:52 +0300

    Feat:add navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,3 +1,4 @@
+import { Search } from '@material-ui/icons'
 import React from 'react'
 import styledComponents from 'styled-components'
 
@@ -8,15 +9,38 @@ const Container = styledComponents.div`
 const Wrapper = styledComponents.div `
     padding: 10px 20px;
     display:flex;
-    justify-content: space-between
+    justify-content: space-between;
+    align-items:center;
+    
     
 `
 
 const Left = styledComponents.div`
-
+  flex: 1;
+  display: flex;
+  align-items: center;
 `
-const Centre = styledComponents.div``
-const Right = styledComponents.div``
+const Language = styledComponents.span`
+  font-size:14px;
+  cursor:pointer;
+`
+const SearchContainer = styledComponents.div`
+  border: 0.5px solid light gray;
+  display: flex;
+  align-items:center;
+  margin-left:25px;
+  padding:5px;
+`
+const Input = styledComponents.input`  
+`
+const Centre = styledComponents.div`
+flex: 1;
+text-align:center;
+`
+const Logo = styledComponents.h1`
+  font-weight: Bold;
+`
+const Right = styledComponents.div`flex: 1;`
 
 
 
@@ -24,8 +48,14 @@ const Navbar = () => {
   return (
     <Container>
         <Wrapper>
-            <Left>Left</Left>
-            <Centre>Centre</Centre>
+            <Left>
+              <Language>EN</Language>
+              <SearchContainer>
+                <Input/>
+                <Search/>
+              </SearchContainer>
+            </Left>
+            <Centre><Logo>VENICA LADY</Logo></Centre>
             <Right>Right</Right>
         </Wrapper>
     </Container>
